8b4aa9669eab6e1e6e6eb2f2c32ee58fd456e882
NikCarnation <coc57749top@gmail.com>
2025-04-21 22:09:34 +1000

    test commit

---
+++ app/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_company():
+    return render_template("about.html")
+
+@app.route("/our_services")
+def company_service():
+    return render_template("company_services.html")
+
+
+@app.route("/partner")
+def company_partner():
+    return render_template("partner.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+    
