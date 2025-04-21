fef26a8b7e80bd88169b3474943bb5ac61c62692
NikCarnation <coc57749top@gmail.com>
2025-04-22 00:10:39 +1000

    add page's and disign

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
@@ -19,6 +19,10 @@ def company_service():
 @app.route("/partner")
 def company_partner():
     return render_template("partner.html")
+
+@app.route("/user/<username>")
+def user(username):
+    return render_template("user.html", user=username) #для уязвимости типа idor. Сделаем тестовую страницу админа/разраба и на нее засунем что нибудь
 
 
 if __name__ == "__main__":
