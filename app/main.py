1e6a21ef8db2c2d85dcbbfd24c9d4a52200f12cc
NikCarnation <coc57749top@gmail.com>
2025-04-23 22:25:54 +1000

    add user page, new style and docker

--- app/main.py
+++ app/main.py
@@ -20,10 +20,17 @@ def company_service():
 def company_partner():
     return render_template("partner.html")
 
-@app.route("/user/<username>")
-def user(username):
-    return render_template("user.html", user=username) #для уязвимости типа idor. Сделаем тестовую страницу админа/разраба и на нее засунем что нибудь
 
+dict_of_employees = {0:"tony",  1: "anna", 2: "bob", 3: "nik", 4: "dima" }
+
+
+
+@app.route("/employees")
+def user():
+    id = request.args.get("id")
+    id = int(id)
+    employee_name = dict_of_employees.get(id, "anna")  
+    return render_template(f"employees/{dict_of_employees[id]}.html", id = id)
 
 if __name__ == "__main__":
     app.run(debug=True)
